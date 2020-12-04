8842775e257aec67178079903eed765921627b1b
root <root@instance-1.us-central1-a.c.siva-297017.internal>
2020-12-04 16:58:54 +0000

    scripts

---
+++ all.sh
@@ -0,0 +1,12 @@
+echo "[+] Fetching subdomains"
+subfinder -d $1 > domains
+echo "[+] Checking http status"
+bash ~/scripts/alive.sh
+
+bash ~/scripts/dnscheck.sh
+bash ~/scripts/iplist.sh
+bash ~/scripts/ports.sh
+bash ~/scripts/altsubs.sh
+bash ~/scripts/alt-httpx.sh
+bash ~/scripts/200_status.sh
+bash ~/scripts/urls.sh
--- dnscheck.sh
+++ dnscheck.sh
@@ -1,1 +1,1 @@
-dnsx -silent -a -cname -resp -l domains -r ../resolvers.txt -o alt-resolved -t 300
+dnsx -silent -a -cname -resp -l domains -r ~/resolvers.txt -o alt-resolved -t 300 -o dns
