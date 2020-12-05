a3a1173ad2dddb51e5e165f7b2c70de403cfa36a
root <root@recon.us-central1-a.c.siva-297017.internal>
2020-12-05 03:36:41 +0000

    fix

--- dnscheck.sh
+++ dnscheck.sh
@@ -1,1 +1,1 @@
-dnsx -silent -a -cname -resp -l domains -r ~/resolvers.txt -o alt-resolved -t 300 -o dns
+dnsx -silent -a -cname -resp -l domains -r ~/scripts/resolvers.txt -o alt-resolved -t 300 -o dns
---
+++ resolvers.sh
@@ -0,0 +1,1 @@
+dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 300 -o ~/scripts/resolvers.txt
