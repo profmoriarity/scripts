730571aa203c164969c4a0335975f8f674dd80d7
root <root@recon.us-central1-a.c.siva-297017.internal>
2020-12-29 06:03:26 +0000

    live test fixes

--- all.sh
+++ all.sh
@@ -1,3 +1,5 @@
+mkdir $1
+cd $1
 bash ~/scripts/init.sh $1 domains-$1
 bash ~/scripts/alive.sh domains httpx-$1
 bash ~/scripts/dnscheck.sh domains dns-$1
@@ -7,7 +9,7 @@ bash ~/scripts/altsubs.sh
 bash ~/scripts/alt-httpx.sh
 bash ~/scripts/200_status.sh
 bash ~/scripts/urls.sh dummy urls-$1
-bash ~/scripts/nuclei-scan.sh domains alive nuclei-domains-$1
+bash ~/scripts/nuclei-scan.sh domains httpx nuclei-domains-$1
 bash ~/scripts/nuclei-scan.sh urls 200-ok nuclei-urls-$1
 bash ~/scripts/green.sh domains-$1
 bash ~/scripts/green.sh httpx-$1
--- urls.sh
+++ urls.sh
@@ -1,3 +1,3 @@
 cat domains | gau | tee urls
 cat urls | httpx -status-code -content-length -title -threads 500 -timeout 3 -json | tee urls-status| jq -s . | ~/scripts/toElastic.py $2
-cat urls-status | jq -s '.[]| select(."status-code" | contains(200)) | .url' | awk -F'"' '{print $2}' | 200-ok
+cat urls-status | jq -s '.[]| select(."status-code" | contains(200)) | .url' | awk -F'"' '{print $2}' | tee 200-ok
